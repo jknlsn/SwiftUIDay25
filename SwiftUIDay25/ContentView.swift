f54858dc99231560e1f37dcd63531b0a483f4fc1
Jake Nelson <j.n@live.com.au>
2020-04-19 14:41:38 +0800

    Initial Commit

---
+++ SwiftUIDay25/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDay25
+//
+//  Created by Jake Nelson on 19/4/20.
+//  Copyright © 2020 Jake Nelson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
